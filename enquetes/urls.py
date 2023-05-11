442135deaab5d2cbe20338f0fc9bcc2d18b6889b
ythairony <yurithairony@hotmail.com>
2023-05-11 12:06:47 +0000

    template da enquete

--- enquetes/urls.py
+++ enquetes/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('enquete/<int:pergunta_id>/', views.detalhes, name='detalhes'),
+    path('enquete/<int:pergunta_id>/votacao/', views.votacao, name='votacao'),
+    path('enquete/<int:pergunta_id>/resultado/', views.resultado, name='resultado'),
 ]
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Pergunta
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Nome: Yuri Thairony Feitosa de Oliveira <br> Matrícula: 20222014040015")
+    lista_perguntas = Pergunta.objects.all()
+    template = loader.get_template('enquetes/index.html')
+    context = {'lista_perguntas': lista_perguntas, }
+    return HttpResponse(template.render(context, request))
+
+
+def detalhes(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+    return render(request, 'enquetes/detalhes.html', {'pergunta':pergunta})
+
+
+def votacao(request, pergunta_id):
+    resposta = "VOTAÇÃO da Enquete de número %s"
+    return HttpResponse(resposta % pergunta_id)
+
+
+def resultado(request, pergunta_id):
+    resposta = "RESULTADOS da Enquete de número %s"
+    return HttpResponse(resposta % pergunta_id)
