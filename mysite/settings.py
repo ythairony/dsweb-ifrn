0c0c6347a215ab27da28696f0ff063ecdad7603f
Yuri Thairony <yurithairony@hotmail.com>
2023-06-23 23:11:21 -0300

    configurando notebook

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-38-@+9xgq*#w*zp0hagd&0+$4qsc@%=3fbr=mn_f2e-ms26@8_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yurithairony.pythonanywhere.com']
+ALLOWED_HOSTS = ['yurithairony.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
