1aa4501937262ff143c77fbcead6857a639bb2d0
ythairony <yurithairony@hotmail.com>
2023-06-29 14:20:38 +0000

    Inserindo os diagramas

--- caridade/admin.py
+++ caridade/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Evento, Item, Reserva
 
-# Register your models here.
+admin.site.site_header = 'EVENTO DE CARIDADE'
+
+admin.site.register(Evento)
+admin.site.register(Item)
+admin.site.register(Reserva)
--- caridade/models.py
+++ caridade/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Evento(models.Model):
+    nome = models.CharField(max_length=100)
+    data_inicio = models.DateField('Data de início')
+    data_fim = models.DateField('Data de encerramento')
+    def __str__(self):
+        return self.nome
+
+class Item(models.Model):
+    imagem = models.ImageField(upload_to='imagem_item/', null=True, blank=True)
+    descricao = models.CharField(max_length=50)
+    valor = models.FloatField()
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, default=1)
+    def __str__(self):
+        return self.descricao
+
+class Reserva(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
