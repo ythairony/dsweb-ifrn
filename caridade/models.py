d0c0464702b05e4d3008aff4c9d91092918ed503
ythairony <yurithairony@hotmail.com>
2023-07-28 20:50:45 +0000

    Corrigindo a associação de usuario com evento e item

--- caridade/models.py
+++ caridade/models.py
@@ -6,6 +6,8 @@ class Evento(models.Model):
     nome = models.CharField(max_length=100)
     data_inicio = models.DateField('Data de início')
     data_fim = models.DateField('Data de encerramento')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return self.nome
 
@@ -14,11 +16,16 @@ class Item(models.Model):
     descricao = models.CharField(max_length=50)
     valor = models.FloatField()
     evento = models.ForeignKey(Evento, on_delete=models.CASCADE, default=1)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
         return self.descricao
 
 class Reserva(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.item.descricao} reservado por: {self.usuario.username}"
 
 
