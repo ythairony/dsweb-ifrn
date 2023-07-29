74df21c880d2a9fd6baddf0d4aa05db24e22b698
ythairony <yurithairony@hotmail.com>
2023-07-29 20:46:36 +0000

    Index implementado

--- caridade/urls.py
+++ caridade/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'caridade'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('caridade/<int:pk>/', views.DetalhesView.as_view(), name='detalhes'),
 ]
--- caridade/views.py
+++ caridade/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from .models import Evento
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'caridade/index.html')
+        lista_eventos = Evento.objects.all()
+        contexto = { 'lista_eventos' : lista_eventos}
+        return render(request, 'caridade/index.html', contexto)
+
+
+class DetalhesView(View):
+    def get(self, request, *args, **kwargs):
+        evento = get_object_or_404(Evento, pk=kwargs['pk'])
+        contexto = {'evento': evento}
+        return render(request, 'caridade/detalhes.html', contexto)
